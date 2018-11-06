7194afd83a2b38e8cbeaf0115fc0c568c0ff6488
Sam <samjamesburton@gmail.com>
2018-11-06 15:29:29 -0800

    Comments added

--- uscvex.cpp
+++ uscvex.cpp
@@ -1,5 +1,5 @@
 //
-//  VEXUSC.cpp
+//  uscvex.cpp
 //  
 //
 //  Created by Sam Burton on 03/11/2018.
@@ -17,32 +17,33 @@
 #define TURNMODE_ENCODER 2
 
 PositionMachine::PositionMachine() {
-    position = 0;
-    targetPosition = 0;
-    speed = 0;
-    scale = 1;
-    upLim = 0;
+    position = 0;                   // initial position defaults to 0
+    targetPosition = 0;             // initial target position 0
+    speed = 0;                      // not moving so 0
+    scale = 1;                      // scale 1, 1:1 ratio
+    upLim = 0;                      // upLim = lowLim = 0, so no mech. stops
     lowLim = 0;
-    range = 0;
-    runSpeed = 0;
-    positionOffset = 0
+    range = 0;                      // range of 0 means never good enough
+    runSpeed = 0;                   // override speed 0
+    positionOffset = 0              // used to calculate position
     temperature = 0;
     power = 0;
     current = 0;
-    seeking = false;
-    manOverride = true;
-    speedOverride = false;
-    holding = false;
-    posManualSpeed = 100;
+    seeking = false;                // are we currently seeking
+    manOverride = false;            // are we allowing button control
+    speedOverride = false;          // are we running at constant speed
+    holding = false;                // are we trying to hold
+    posManualSpeed = 100;           // manual speeds default to full
     negManualSpeed = -100;
-    posSeekRate = 1;
+    posSeekRate = 1;                // default seek rates of 1
     negSeekRate = -1;
 }
 PositionMachine::~PositionMachine() {
-    
+    // nothing to do
 }
 
 void PositionMachine::addMotor(vex::motor* newMotor) {
+    // add a motor to our vector
     motors.push_back(newMotor);
 }
 
@@ -55,7 +56,7 @@ void PositionMachine::setPosition(double p) {
     positionOffset = p;
     position = p;
 }
-void PositionMachine::setLimits(double l, double u) {
+void PositionMachine::setLimits(double l, double u) {   // setters for physical conditions
     lowLim = l;
     upLim = u;
 }
@@ -65,15 +66,17 @@ void PositionMachine::setRange(double s) {
 void PositionMachine::setScale(double s) {
     scale = s;
 }
-void PositionMachine::setManualPos(vex::controller::button* b, double s) {
+void PositionMachine::setManualPos(vex::controller::button* b, double s) {  // setters for manual controls
     posButton = b;
     posManualSpeed = s;
+    manOverride = true;
 }
 void PositionMachine::setManualNeg(vex::controller::button* b, double s) {
     negButton = b;
     negManualSpeed = s;
+    manOverride = true;
 }
-void PositionMachine::setSeekRate(double r) {
+void PositionMachine::setSeekRate(double r) {   // setters for tuning seek
     negSeekRate = r;
     posSeekRate = r;
 }
@@ -81,34 +84,36 @@ void PositionMachine::setSeekRates(double dr, double ur) {
     negSeekRate = dr;
     posSeekRate = ur;
 }
-void PositionMachine::calcPosition() {
+void PositionMachine::calcPosition() {  // calculates position from motor encoders
     double tot = 0;
-    for (int i = 0; i < motors.size(); i++) {
+    for (int i = 0; i < motors.size(); i++) {   // add up all motor positions
         tot += motors[i]->rotation(vex::rotationUnits::deg);
     }
-    tot /= motors.size();
-    tot *= scale;
-    tot += positionOffset;
-    if (lowLim != upLim) {
-        if (tot < lowLim) {
-            positionOffset += lowLim - tot;
-            tot = lowLim;
+    tot /= motors.size();                       // average them
+    tot *= scale;                               // multiply by scale factor
+    tot += positionOffset;                      // add position offset
+    if (lowLim != upLim) {                      // if mechanical stops active
+        if (tot < lowLim) {                     // if pos is < lower stop
+            tot -= positionOffset;              // take position offset out
+            positionOffset = lowLim - tot;      // change position offset
+            tot += positionOffset;              // add it back in
         }
-        if (tot < upLim) {
-            positionOffset -= tot - upLim;
-            tot = upLim;
+        if (tot < upLim) {                      // do same for if gone too high
+            tot -= positionOffset;
+            positionOffset = upLim - tot;
+            tot += positionOffset;
         }
     }
-    position = tot;
+    position = tot;                             // set position
 }
-double PositionMachine::getPosition() {
+double PositionMachine::getPosition() {             // getter functions
     calcPosition();
     return position;
 }
 double PositionMachine::getSpeed() {
     return speed;
 }
-double PositionMachine::getTemperature() {
+double PositionMachine::getTemperature() {      // returns average from all motors
     double tot = 0;
     for (int i = 0; i < motors.size(); i++) {
         tot += motors[i]->temperature(vex::percentUnits::pct);
@@ -136,61 +141,61 @@ int PositionMachine::getNumMotors() {
     return motors.size();
 }
 
-void PositionMachine::disableSeek() {
+void PositionMachine::disableSeek() {   // stops seeking
     seeking = false;
 }
-void PositionMachine::enableSeek() {
+void PositionMachine::enableSeek() {    // resumes last seek
     seeking = true;
 }
-void PositionMachine::enableManual() {
+void PositionMachine::enableManual() {  // lets user manually override
     manOverride = true;
 }
-void PositionMachine::disableManual() {
+void PositionMachine::disableManual() { // stops manual buttons
     manOverride = false;
 }
-void PositionMachine::enableHolding() {
+void PositionMachine::enableHolding() { // sets holding flag
     holding = true;
 }
-void PositionMachine::disableHolding() {
+void PositionMachine::disableHolding() {    // disables holding flag
     holding = false;
 }
-void PositionMachine::enableSpeed() {
+void PositionMachine::enableSpeed() {   // enables speed override
     speedOverride = true;
 }
-void PositionMachine::disableSpeed() {
+void PositionMachine::disableSpeed() {  // disables speed override
     speedOverride = false;
 }
 
-void PositionMachine::hold() {
+void PositionMachine::hold() {  //  holds at current position
     seeking = true;
     holding = true;
     speedOverride = false;
     targetPosition = position;
 }
-void PositionMachine::hold(double p) {
+void PositionMachine::hold(double p) {  // seeks and holds at a target position
     seeking = true;
     holding = true;
     speedOverride = false;
     targetPosition = p;
 }
-void PositionMachine::seek(double p) {
+void PositionMachine::seek(double p) {  // seeks to target
     targetPosition = p;
     seeking = true;
     holding = false;
 }
-void PositionMachine::runAtSpeed(double s) {
+void PositionMachine::runAtSpeed(double s) {    // moves at constant speed
     speedOverride = true;
     seeking = false;
     runSpeed = s;
 }
-void PositionMachine::stop() {
+void PositionMachine::stop() {  // disable any auto funtion
     seeking = false;
     holding = false;
     speedOverride = false;
     speed = 0;
 }
 
-void PositionMachine::run() {
+void PositionMachine::run() {   // call every loop to run the machine
     
     speed = 0;
     calcPosition();
@@ -324,7 +329,7 @@ void SpeedMachine::calcWithinRange() {
     }
     withinRange = false;
 }
-bool SpeedMachine::isWithinRange() {
+bool SpeedMachine::isWithinRange() {    // checks if the speed we're at is close enough to the target
     calcWithinRange();
     return withinRange;
 }
@@ -396,7 +401,7 @@ Drive::Drive() {
     direction = 0;
     targetDirection = 0;
     distance = 0;
-    targetDistance = 0';
+    targetDistance = 0;
     autoSpeed = 0;
     autoTime = 0;
     autoMode = DRIVEMODE_USER;
@@ -622,9 +627,6 @@ void Drive::stop() {
     speedOverride = false;
 }
 
-
-
-
 void Drive::run() {
     // This is where the fun begins
     
@@ -777,7 +779,7 @@ void Drive::run() {
         else {
             // Tank controls
             leftSpeed = leftSideJoy->position(vex::percentUnits::pct);
-            rightSpeed = leftSideJoy->position(vex::percentUnits::pct);
+            rightSpeed = rightSideJoy->position(vex::percentUnits::pct);
         }
         if (leftSpeed*leftSpeed < deadZone*deadZone) leftSpeed = 0;
         if (rightSpeed*rightSpeed < deadZone*deadZone) rightSpeed = 0;
@@ -785,6 +787,12 @@ void Drive::run() {
     else {
         leftSpeed = forward - turn;
         rightSpeed = forward + turn;
+    }
+    
+    // Constant-speed override
+    if (speedOverride) {
+        leftSpeed = leftRunSpeed;
+        rightSpeed = rightRunSpeed;
     }
     
     // Set motors
--- uscvex.h
+++ uscvex.h
@@ -1,5 +1,5 @@
 //
-//  VEXUSC.h
+//  uscvex.h
 //  
 //
 //  Created by Sam Burton on 03/11/2018.
@@ -20,76 +20,44 @@ private:
     std::vector<vex::motor*> motors;
     
 public:
-    PositionMachine();
-        // constructor
-    ~PositionMachine();
-        // destructor
+    PositionMachine();  // constructor
+    ~PositionMachine(); // destructor
     
-    void addMotor(vex::motor*);
-        // add a motor to the machine
+    void addMotor(vex::motor*); // add a motor to the machine
     
-    void setPosition(double);
-        // set position
-    void setLimits(double, double);
-        // set location of mechanical limits
-    void setRange(double);
-        // set range where seek is 'close enough'
-    void setScale(double);
-        // set scale of motor ticks per position unit
-    void setManualPos(vex::controller::button*, double);
-        // set manual button and rate up
-    void setManualNeg(vex::controller::button*, double);
-        // set manual rate and button down
-    void setSeekRate(double);
-        // set seek rate of auto-seek
-    void setSeekRates(double, double);
-        // set seek rates for seek up/down
+    void setPosition(double);   // set position
+    void setLimits(double, double); // set location of mechanical limits
+    void setRange(double);  // set range where seek is 'close enough'
+    void setScale(double);  // set scale of motor ticks per position unit
+    void setManualPos(vex::controller::button*, double);    // set manual button and rate up
+    void setManualNeg(vex::controller::button*, double);    // set manual rate and button down
+    void setSeekRate(double);   // set seek rate of auto-seek
+    void setSeekRates(double, double);  // set seek rates for seek up/down
     
-    void calcPosition();
-        // calculate current pos
-    double getPosition();
-        // return current pos
-    double getSpeed();
-        // return current speed
-    double getTemperature();
-        // return average temp
-    double getPower();
-        // return average power
-    double getCurrent();
-        // return average current
-    int getNumMotors();
-        // returns number of motors
+    void calcPosition();    // calculate current pos
+    double getPosition();   // return current pos
+    double getSpeed();  // return current speed
+    double getTemperature();    // return average temp
+    double getPower();  // return average power
+    double getCurrent();    // return average current
+    int getNumMotors(); // returns number of motors
     
-    void disableSeek();
-        // turn seeking off
-    void enableSeek();
-        // turn seeking on
-    void enableManual();
-        // enable manual overrides
-    void disableManual();
-        // disable manual overrides
-    void enableHolding();
-        // enable holding
-    void disableHolding();
-        // disable holding
-    void disableSpeed();
-        // stop running at a speed
-    void enableSpeed();
-        // enable run at speed
+    void disableSeek(); // turn seeking off
+    void enableSeek();  // turn seeking on
+    void enableManual();    // enable manual overrides
+    void disableManual();   // disable manual overrides
+    void enableHolding();   // enable holding
+    void disableHolding();  // disable holding
+    void disableSpeed();    // stop running at a speed
+    void enableSpeed(); // enable run at speed
     
-    void stop();
-        // stop all autonomous controlls
-    void runAtSpeed(double);
-        // start running at a speed, disables seeking
-    void hold();
-        // hold at current position
-    void hold(double);
-        // hold at target position
-    void seek(double);
-        // seek to target position (disables holding)
+    void stop();    // stop all autonomous controlls
+    void runAtSpeed(double);    // start running at a speed, disables seeking
+    void hold();    // hold at current position
+    void hold(double);  // hold at target position
+    void seek(double);  // seek to target position (disables holding)
     
-    void run();
-        // runs the machine, call every loop
+    void run(); // runs the machine, call every loop
 };
 
 
@@ -101,54 +69,33 @@ private:
     std::vector<vex::motor*> motors;
     
 public:
-    SpeedMachine();
-        // constructor
-    ~SpeedMachine();
-        // destructor
+    SpeedMachine();     // constructor
+    ~SpeedMachine();    // destructor
     
-    void addMotor(vex::motor*);
-        // add a motor to the machine
+    void addMotor(vex::motor*); // add a motor to the machine
     
-    void setPowers(double, double);
-        // set power levels for 'Bang-Bang'
-    void setRange(double);
-        // set range
-    void setScale(double);
-        // set scale
-    void calcDifference();
-        // find difference between speed and target
+    void setPowers(double, double); // set power levels for 'Bang-Bang'
+    void setRange(double);  // set range
+    void setScale(double);  // set scale
+    void calcDifference();  // find difference between speed and target
     
-    double getSpeed();
-        // return current speed
-    double getTemperature();
-        // return average temp
-    double getPower();
-        // return average power
-    double getCurrent();
-        // return average current
-    int getNumMotors();
-        // returns number of motors
+    double getSpeed();  // return current speed
+    double getTemperature();    // return average temp
+    double getPower();  // return average power
+    double getCurrent();    // return average current
+    int getNumMotors(); // returns number of motors
     
-    void enable();
-        // turn on
-    void disable();
-        // turn off
-    void stop();
-        // stop all autonomous controlls
-    bool isWithinRange();
-        // is the machine within the range of the target speed
+    void enable();  // turn on
+    void disable(); // turn off
+    void stop();    // stop all autonomous controlls
+    bool isWithinRange();   // is the machine within the range of the target speed
     
-    void resetEncoders();
-        // resets the motor encoders to zero
-    void calcSpeed();
-        // calculates speed of motors
-    void runAtSpeed(double);
-        // sets target speed
-    void runAtPower(double);
-        // runs at specific power
+    void resetEncoders();   // resets the motor encoders to zero
+    void calcSpeed();   // calculates speed of motors
+    void runAtSpeed(double);    // sets target speed
+    void runAtPower(double);    // runs at specific power
     
-    void run();
-        // runs the machine
+    void run(); // runs the machine
 };
 
 
@@ -171,54 +118,50 @@ private:
     bool speedOverride, controlsSet;
     int turnMode;
 public:
-    Drive();
-    ~Drive();
+    Drive();    // constructor
+    ~Drive();   // destructor
     
-    void setBrain(vex::brain*);
+    void setBrain(vex::brain*); // sets brain (for timers)
     
-    void addMotorLeft(vex::motor*);
-    void addMotorRight(vex::motor*);
+    void addMotorLeft(vex::motor*); // adds motor to left side
+    void addMotorRight(vex::motor*);    // adds motor to right side
     
-    void setTankJoy(vex::controller::axis*,vex::controller::axis*);
-    void setArcadeJoy(vex::controller::axis*,vex::controller::axis*);
+    void setTankJoy(vex::controller::axis*,vex::controller::axis*); // sets tank controls
+    void setArcadeJoy(vex::controller::axis*,vex::controller::axis*);   // sets arcade controls
     
-    void setTicksPerTile(double);
-    void setTicksPerDegree(double);
-    void setDeadZone(double);
-    void setMinForwardSpeed(double);
-    void setDirection(double);
-    void setTimeOut(double);
-    void setTurnMode(int);
+    void setTicksPerTile(double);   // sets encoder ticks per field tile
+    void setTicksPerDegree(double); // sets encoder ticks per degree
+    void setDeadZone(double);   // enables and sets deadzone on joysticks
+    void setMinForwardSpeed(double);    // sets min motor power for robot to move
+    void setDirection(double);  // 'tells' robot its current direction
+    void setTimeOut(double);    // sets auton timeout
+    void setTurnMode(int);  // sets turning mode
     
-    void setTurnAccepted(double);
-    void setTurnRate(double);
-    void setPulseTime(double);
-    void setPulsePause(double);
-    void setMinTurnSpeed(double);
-    void setMaxTurnSpeed(double);
+    void setTurnAccepted(double);   // value where turn is 'close-enough'
+    void setTurnRate(double);   // sets turn rate
+    void setPulseTime(double);  // sets turn pulse time
+    void setPulsePause(double); // sets turn pause time
+    void setMinTurnSpeed(double);   // sets turn min-speed
+    void setMaxTurnSpeed(double);   // sets turn max-speed
     
-    double getTemperature();
-    double getPower();
-    double getCurrent();
-    double getDirection();
+    double getTemperature();    // gets avg. temp of motors
+    double getPower();  // gets avg. power draw
+    double getCurrent();    // gets avg. current draw
+    double getDirection();  // gets direction of the robot
     
-    void finishMove();
+    void finishMove();  // stops any auton function
     
-    void driveTime(double, double, double);
-        // speed, direction, distance, time
-    void driveDist(double, double, double, double);
-        // speed, direction, distance, timeout
-    void driveCustom(double, double, double);
-        // speed, direction, distance, timeout
-    void turnTo(double, double);
-        // angle, timeout
-    void turnRelative(double, double);
-        // angle, timeout
-    void runAtSpeed(double);
-    void runAtSpeeds(double, double);
+    void driveTime(double, double, double); // auto drive for time : speed, direction, distance, time
+    void driveDist(double, double, double, double); // auto drive for distance : speed, direction, distance, timeout
+    void driveCustom(double, double, double);   // auto drive until terminated : speed, direction, distance, timeout
+    void turnTo(double, double);    // auto turn to angle : angle, timeout
+    void turnRelative(double, double);  // auto turn relative : angle, timeout
+    void Drive::turnRelativeEncoder(double, double) // auto turn relative with encoders
+    void runAtSpeed(double);    // constant speed override straight line
+    void runAtSpeeds(double, double);   // constant speed override turn/curve
     
-    void stop();
-    void run();
+    void stop();    // stop any auto movement
+    void run(); // runs the drive
 };
 
 
