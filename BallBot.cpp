477fcbbf089212d0fc0549994fe5878715eb1e94
Sam <samjamesburton@gmail.com>
2018-11-08 13:02:02 -0800

    fleshed-out runFlywheel()

--- BallBot.cpp
+++ BallBot.cpp
@@ -5,9 +5,42 @@
 vex::competition Competition;
 
 // Declare objects as globals
+Drive drive = Drive();
+SpeedMachine flyWheel = SpeedMachine();
+Motor intakeInner, intakeOuter;
+
+// #defines for controller buttons
+#define BTN_FIRE_LOW
+#define BTN_FIRE_HIGH
+#define BTN_FIRE_BOTH
+#define BTN_FIRE_PRESET
+#define BTN_INTAKE_IN
+#define BTN_INTAKE_OUT
+#define BTN_INTAKE_TOGGLE
+#define BTN_ABORT
+
+// #defines for tuning
+#define FLYWHEEL_AIM_RANGE 5    // fire ball when within x degrees of flag
 
 // Declare and initialize any global flags we need:
-// autoAimEnabled, autoFireEnabled, flywheelTargetSpeed, etc.
+int autoFireState = -1;         // -1 for neutral, 1 for 'aim&spin&fire', 2 for 'spin & fire', 3 for 'fire!'
+int targetFlag = 1;             // 1 for low, 2 for high, 3 for high then low
+double intakeSpeedOuter = 0;    // speed for outer intake
+double intakeSpeedInner = 0;    // speed for inner intake
+double flyWheelDefaultSpeed = 0;    // set speed for fixed-dist fireing
+int runTillBall = 0;            // 0 = nothing, 1 = run till 1 ball in, 2 = run for two balls
+
+// Array for flywheel speed lookup'
+// Distance (tiles), low flag speed (rpm), high flag speed (rpm)
+// For each distance we record flywheel speeds needed for hitting high/low flags
+double[][] flywheelSpeeds = {
+    {-100, 0, 0},   // to catch errors
+    {0, 25, 50},
+    {0.5, 100, 125},
+};
+int flyWheelSpeedsDefinition = 3;   // number of entries
+
+
 
 void pre_auton( void ) {
     // Set-up SpeedMachine for flywheel
@@ -18,67 +51,181 @@ void pre_auton( void ) {
 int runFlywheel() {
     
     // Declare any local variables
+    bool ballIsIn = false;
+    bool ballWasIn = false;
+    bool justToggledAutoBall = false;
     
     while (true) {
         
         // Set intake motor speeds to 0
+        intakeSpeedInner = 0;
+        intakeSpeedOuter = 0;
+
+        // keep flywheel at default speed
+        flywheel.runAtSpeed(flyWheelDefaultSpeed);
         
-
-
-        if (/*  auto-aim flag is set    */) {
+        if (autoFireState != -1) {  // Auto fire
             
             // Check vision sensor to determine necessary turn
+            double relativeAngle = 0;
+            if (autoFireState <= 1) {
+                // Read sensor and find relative angle
+                
+                
+            }
             
+            // Check lidar / ultrasonic for distance
+            double distance = 0;
+            if (autoFireState <= 2) {
+                // Read sensor and find distance
+                
+                
+            }
             
-            // Check lidar / ultrasonic for target flywheel speed
-            
+            // Lookup distance in flywheelSpeeds table, & interpolate to find speed
+            double targetSpeed = flyWheelDefaultSpeed;
+            if (autoFireState <= 2) {
+                int index = -1;
+                for (int i = 1; i < flyWheelSpeedsDefinition; i++) {
+                    // Look for speed too high
+                    if (flyWheelSpeeds[i][0] >= distance) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index == -1) {
+                    // Further than furthest in table
+                    if (targetFlag == 1) targetSpeed = flyWheelSpeeds[i][1];
+                    else targetSpeed = flyWheelSpeeds[i][2];
+                }
+                else {
+                    // Interpolate for correct speed
+                    // find how similar distance is to each value
+                    double distDiff = (distance - flyWheelSpeeds[index-1][0]);
+                    distDiff /= (flyWheelSpeeds[index][0] - flyWheelSpeeds[index-1][0]);
+                    double speedDiff;
+                    // Find how similar speed should be to each value & set target speed
+                    if (targetFlag == 1) {
+                        speedDiff = (flyWheelSpeeds[index][1] - flyWheelSpeeds[index-1][1]);
+                        targetSpeed = speedDiff*distDiff + flyWheelSpeeds[index-1][1];
+                    }
+                    else {
+                        speedDiff = (flyWheelSpeeds[index][2] - flyWheelSpeeds[index-1][2]);
+                        targetSpeed = speedDiff*distDiff + flyWheelSpeeds[index-1][2];
+                    }
+                }
+            }
             
             // Read flags for & set flywheel object target speed
-            
+            flyWheel.runAtSpeed(targetSpeed);
             
             // Read vision sensor & ask drive to turn appropriately
+            drive.turnRelative(relativeAngle);
             
-
-            // Check current speed of flywheel
-            
-            
-            if (/*  within accepted aim error & flywheel at correct speed   */) {
+            bool fireBall = false;
+            // Check current speed of flywheel & if aimed
+            if ((flywheel.isWithinRange() && abs(relativeAngle) < FLYWHEEL_AIM_RANGE)) {
                 // Set flag for fireing ball
+                fireBall = true;
             }
             
-        }
-        
-        
-        if (/*  flag set for fireing ball   */) {
-            // Set intake motor speed
+            // Check if ball is in ready position
+            ballIsIn = false;
+            // read sensors to check if ball is in
             
             
-            if (/*  ball launched   */) {
-                // Clear flags
+            
+            if (!ballIsIn && !ballWasIn) {    // move ball into position if not already
+                intakeSpeedOuter = 100;
+                intakeSpeedInner = 25;
+            }
+            
+            if (fireBall) {         // aimed and running correct speed
+                // Run intake motor
+                intakeSpeedInner = 100;
+                
+                if (ballWasIn && !ballIsIn) {   // ball has left
+                    // Clear flags
+                    fireBall = false;
+                    if (targetFlag == 3) {  // wanted to shoot high then low
+                        targetFlag = 1;
+                    }
+                    else {
+                        autoFireState = -1;
+                    }
+                }
             }
             
             
-        }
+        }   // end of auto-fire
+        
+        
         
         // Check controller buttons...
         // Set flags for preset flywheel speeds & auto-aim-fire
-        // If manual intake buttons pressed, set intake speeds
+        // If manual intake buttons pressed, override intake speeds
         
-        // Buttons to run intake (For/Rev)
-        // Buttons for auto aim & fire (High/Low)
-        // Buttons for runing flywheel at preset speeds (2)
-        // Button to clear all flags, i.e. stop auto functions
-        
-        if (/*  preset speed wanted */) {
-            // Send target speed to flywheel
-            // flywheel.runAtSpeed(speed);
-            
+        if (BTN_FIRE_LOW) { // auto fire low
+            autoFireState = 1;
+            targetFlag = 1;
+        }
+        if (BTN_FIRE_HIGH) { // auto fire high
+            autoFireState = 1;
+            targetFlag = 2;
+        }
+        if (BTN_FIRE_BOTH) { // auto fire both
+            autoFireState = 1;
+            targetFlag = 3;
+        }
+        if (BTN_FIRE_PRESET) { // auto fire preset
+            autoFireState = 3;
         }
         
+        if (BTN_INTAKE_IN) { // manual run intake in
+            intakeSpeedInner = 25;
+            intakeSpeedOuter = 100;
+            runTillBall = 0;
+        }
+        if (BTN_INTAKE_OUT) { // manual run intake out
+            intakeSpeedInner = -25;
+            intakeSpeedOuter = -100;
+            runTillBall = 0;
+        }
+        if (BTN_INTAKE_TOGGLE) { // toggle auto ball intake
+            if (!justToggledAutoBall) {
+                if (runTillBall) runTillBall = 0; else runTillBall = 2;
+            }
+            justToggledAutoBall = true;
+        }
+        else {
+            justToggledAutoBall = false;
+        }
+        if (BTN_ABORT) {     // cancel auto functions
+            autoFireState = -1;
+            runTillBall = 0;
+        }
+        
+        if (runTillBall) {
+            if (/*read inner sensor*/) {     // ball is not all the way in
+                intakeSpeedOuter = 100;
+                intakeSpeedInner = 25;
+            }
+            else if (/*read outer sensor*/ && (runTillBall == 2)) {   // 1 ball is in, but not 2
+                intakeSpeedOuter = 100;
+            }
+        }
+        
+        
         // Send speeds to intake motors
+        intakeInner.spin(intakeSpeedInner);
+        intakeOuter.spin(intakeSpeedOuter);
         
+        // Run the flywheel
+        flyWheel.run();
         
-        // flywheel.run();
+        // Remember ball info for fireing
+        ballWasIn = ballIsIn;
+        
         
         vex::task::sleep(20);   // don't hog cpu
     }
@@ -142,9 +289,7 @@ void usercontrol( void ) {
     }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
+// main() runs competition/field interaction code... DO NOT CHANGE!!
 int main() {
     
     //Run the pre-autonomous function.
